cb0e768b3c2705ffdf481340e07829ee32bba27a
NktAts <nktats@gmail.com>
2020-10-03 11:26:59 +0900

    fix: 空白を追加し可読性向上

--- udon_soba.bat
+++ udon_soba.bat
@@ -3,11 +3,13 @@ echo off
 setlocal enabledelayedexpansion
 chcp 65001
 echo 環境変数の設定完了
+
 :tempFolderDelete
 if exist .\temporary\*.* (
 	del /Q .\temporary
 	echo 残留ファイル削除完了
 )
+
 :InputCheck
 set filenum=0
 for %%F in (.\input\*.zip) do (set /a filenum=filenum+1)
@@ -16,6 +18,7 @@ if not %filenum%==1 (
 	goto NGExit
 )
 echo フォルダ事前チェック完了
+
 :LoadChatTabConfig
 set n=-1
 for /f %%a in (.\input\chat-tab.txt) do (
@@ -26,12 +29,14 @@ set /a tabnum=n+1
 echo タブ数=%tabnum%
 for /l %%b in (0,1,%n%) do ( echo !tabname[%%b]!)
 echo チャットタブ出力設定読み込み完了
+
 :Unzip
 .\assets\tools\7za.exe x .\input\*.zip -o.\temporary\
 echo 一時フォルダに解凍完了
 :DefaultCopy
 copy .\assets\images\ .\temporary
 echo デフォルト画像コピー完了
+
 :DefineOutputFolder
 set time_tmp=%time: =0%
 set now=%date:/=%%time_tmp:~0,2%%time_tmp:~3,2%%time_tmp:~6,2%
@@ -65,13 +70,16 @@ for /l %%c in (0,1,%n%) do (
 )
 .\assets\tools\msxsl.exe .\temporary\chat.xml .\assets\xslt\convert.xsl -o %outputFolder%\converted_chatlog.html 
 echo HTML出力完了
+
 :Deletezipdata
 del /Q .\temporary
 echo 一時フォルダ内ファイル削除完了
+
 :OKExit
 echo 正常終了
 pause
 exit /b 0
+
 :NGExit
 echo 異常終了
 pause
